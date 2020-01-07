5180b62f7fee2063de454947f416fe4ca8184d09
Thomas Misson <11tmisson@gmail.com>
2020-01-07 22:14:49 +0000

    FINISHED

--- src/solution/Scheduler.java
+++ src/solution/Scheduler.java
@@ -1,5 +1,6 @@
 package solution;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -32,19 +33,25 @@ public class Scheduler implements IScheduler {
 		Schedule s = new Schedule(route, startDate, endDate);
 		s.sort();
 				
-		List<Aircraft> aircrafts = aircraft.getAllAircraft();
-		List<Crew> crewMember = crew.getAllCrew();
-		List<Route> routes = route.getAllRoutes();
-		List<CabinCrew> c;
-				
+		ArrayList<Aircraft> UnAlloaircrafts = new ArrayList<>();
+		ArrayList<CabinCrew> cabinCrew = new ArrayList<>();
+		ArrayList<Pilot> pilots = new ArrayList<>();
+		
 		Random rand = new Random(); 
+		
+		UnAlloaircrafts.addAll(aircraft.getAllAircraft());
+		cabinCrew.addAll(crew.getAllCabinCrew());
+		pilots.addAll(crew.getAllPilots());
+
+		
 		int passes = 0;
 		int lastRemaining = 0;
-		Boolean needsReset = false;
+		Boolean needsReset = false; 
 		
-		int maxPasses = 1000; 
-		
-		
+		Boolean error_messages = false;  // Enable Error messages e.g PrintStack trace
+		int maxPasses = 10000; // Max number of times it'll try to allocate before resetting
+	
+
 		while(!s.isCompleted()) {
 			
 			if(s.getRemainingAllocations().size() == lastRemaining) {
@@ -54,67 +61,127 @@ public class Scheduler implements IScheduler {
 				passes = 0;
 			}
 			
-			if(passes>=maxPasses) {
+			if(passes>maxPasses) {
 				passes = 0;
 				needsReset = true;
 			}
 			
 			if(needsReset) {
 				s = new Schedule(route, startDate, endDate);
+				s.sort();
+				UnAlloaircrafts = new ArrayList<>();
+				cabinCrew = new ArrayList<>();
+				pilots = new ArrayList<>();
+				UnAlloaircrafts.addAll(aircraft.getAllAircraft());
+				cabinCrew.addAll(crew.getAllCabinCrew());
+				pilots.addAll(crew.getAllPilots());
 				needsReset = false;
 			}
 			
-			List<FlightInfo> unallocatedFlights = s.getRemainingAllocations();
+			UnAlloaircrafts.addAll(aircraft.getAllAircraft());
+			
+			if(s.getRemainingAllocations().size()==0)
+			{
+				break;
+			}
+			
+			for(FlightInfo fi : s.getRemainingAllocations()) {
+			
+					Route fiRoute = fi.getFlight();
 
-			for(FlightInfo flight : unallocatedFlights) {
-				Route r = flight.getFlight();
-						
-				try {
-					s.allocateCaptainTo(crew.getAllPilots().get(rand.nextInt(crew.getAllPilots().size())), flight);
+					ArrayList<Aircraft> validPlanes = new ArrayList<>();	
+								
 					
-				}
-				catch(Exception e)
-				{
-				}
-				try {
-					s.allocateCabinCrewTo(crew.getAllCabinCrew().get(rand.nextInt(crew.getAllCabinCrew().size())), flight);
-				}
-				catch(Exception e)
-				{
-				}
-				try {
-					s.allocateFirstOfficerTo(crew.getAllPilots().get(rand.nextInt(crew.getAllPilots().size())), flight);
-				}
-				catch(Exception e)
-				{
-				}
-				try {
-					s.allocateAircraftTo(aircrafts.get(rand.nextInt(aircrafts.size())), flight);
-				} catch (Exception e) {
-				}
+					for (Aircraft ac : aircraft.getAllAircraft()) {
+						
+						if (fiRoute.getDepartureAirportCode().contains(ac.getStartingPosition())) {
+							// TODO: check if the aircraft hasn't been allocated				
+							if ( UnAlloaircrafts.contains(ac) ) {
+								validPlanes.add(ac);
+							}
+							
+						}
+						
+					}
 				
+					
+					for (FlightInfo fl_ac : s.getCompletedAllocations()) {
+						if (fl_ac.getFlight().getDepartureAirportCode().contains(fl_ac.getFlight().getDepartureAirportCode())) {
+							validPlanes.add(s.getAircraftFor(fl_ac));
+						}
+					}
+					
+					if (validPlanes.size() == 0) {
+						continue;
+					} else {
+						try {
+							s.allocateAircraftTo(validPlanes.get(rand.nextInt(validPlanes.size())), fi);
+							UnAlloaircrafts.remove(s.getAircraftFor(fi));
+						} catch (DoubleBookedException e) {
+							if (error_messages) {
+	
+								System.out.println("1 - Double Booked Plane!");
+							}
+						}
+					} 
+		
 				try {
-					s.completeAllocationFor(flight);
+					s.allocateCabinCrewTo(crew.findCabinCrewByTypeRating(s.getAircraftFor(fi).getTypeCode()).get(rand.nextInt(crew.findCabinCrewByTypeRating(s.getAircraftFor(fi).getTypeCode()).size())), fi);
+				} catch (Exception e1) {
+					try {
+						s.allocateCabinCrewTo(crew.getAllCabinCrew().get(rand.nextInt(crew.getAllCabinCrew().size())), fi);
+					} catch (DoubleBookedException e) {
+						if (error_messages) {
+							System.out.println("2 - Double Booked CabinCrew!");
+						}
+					}
+				}
+		
+				try {
+					s.allocateCaptainTo(crew.findPilotsByTypeRating(s.getAircraftFor(fi).getTypeCode()).get(rand.nextInt(crew.findPilotsByTypeRating(s.getAircraftFor(fi).getTypeCode()).size())), fi);
+				} catch (Exception e1) {
+					try {
+						s.allocateCaptainTo(crew.getAllPilots().get(rand.nextInt(crew.getAllPilots().size())), fi);
+					} catch (DoubleBookedException e) {
+						if (error_messages) {
+					
+							System.out.println("3 - Double Booked Pilot!");
+						}
+					
+					}
+				}
+		
+					try {
+						s.allocateFirstOfficerTo(crew.findPilotsByTypeRating(s.getAircraftFor(fi).getTypeCode()).get(rand.nextInt(crew.findPilotsByTypeRating(s.getAircraftFor(fi).getTypeCode()).size())), fi);
+					} catch (Exception e1) {
+						try {
+							s.allocateFirstOfficerTo(crew.getAllPilots().get(rand.nextInt(crew.getAllPilots().size())), fi);
+						} catch (DoubleBookedException e) {
+							if (error_messages) {
+					
+								System.out.println("4 - Double Booked Crew!");
+							}
+						}
+					}
+
+				try {
+					if(s.isValid(fi)) {
+						s.completeAllocationFor(fi);
+					}
+				
 				} catch (InvalidAllocationException e) {
 					
 				}
+				
+				
 			}
-			System.out.println(s.getCompletedAllocations().size() + " Done | Left: " + s.getRemainingAllocations().size());
+			
+			System.out.println("Done: " + s.getCompletedAllocations().size() + " Left: " + s.getRemainingAllocations().size());
 		}
-		
-		QualityScoreCalculator score = new QualityScoreCalculator(aircraft, crew, passenger, s);
-						
-		System.out.println("\nFinished Loading Schedule! With Score : " + score.calculateQualityScore());
-		
-		for(String des : score.describeQualityScore()) {
-			System.out.println(des);
-		}
-		
-		System.out.println(s.getCompletedAllocations().size() + " Done | Left: " + s.getRemainingAllocations().size());
 		
 		return s;
 	}
-
+	
 	@Override
 	public void setSchedulerRunner(SchedulerRunner arg0) {
 		// TODO Auto-generated method stub
